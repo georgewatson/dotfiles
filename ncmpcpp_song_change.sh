8d41894dc421a15b344df3f4091993dbb5f4613e
George Watson <george@georgewatson.me>
2018-12-11 15:31:33 +0000

    Improve ncmpcpp now playing notifications

--- ncmpcpp_song_change.sh
+++ ncmpcpp_song_change.sh
@@ -4,5 +4,7 @@
 IFS=$'\t' read album artist title \
   <<< "$(mpc --format="%album%\t%artist%\t%title%")"
 
-notify-send --urgency=low --expire-time=5000 --app-name=ncmpcpp \
-  --icon=audio-x-generic "$album" "$artist\n$title"
+~/dotfiles/notify-send.sh/notify-send.sh \
+    --urgency=low --expire-time=5000 --app-name=ncmpcpp \
+    --replace-file=/tmp/ncmpcpp_notification \
+    --icon=audio-x-generic "$title" "$artist\n$album"
