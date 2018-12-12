bcf3a0678c2a09aaa5c11a661776a8d2625d01c8
George Watson <george@georgewatson.me>
2018-12-12 16:16:19 +0000

    Fix formatting of notifications for songs containing an ampersand

--- ncmpcpp_song_change.sh
+++ ncmpcpp_song_change.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
 # Get fields from mpc, split by tabs.
-IFS=$'\t' read album artist title \
+IFS=$'\t' read album_raw artist_raw title_raw \
   <<< "$(mpc --format="%album%\t%artist%\t%title%")"
+
+title=$(sed 's/&/and/g' <(echo "$title_raw"))
+artist=$(sed 's/&/and/g' <(echo "$artist_raw") | sed 's/;/, /g')
+album=$(sed 's/&/and/g' <(echo "$album_raw"))
 
 ~/dotfiles/notify-send.sh/notify-send.sh \
     --urgency=low --expire-time=5000 --app-name=ncmpcpp \
--- volume.sh
+++ volume.sh
@@ -30,8 +30,8 @@ function send_notification {
     bar=$(seq -s "─" $(($volume)) | sed 's/[0-9]//g')
     bar_after=$(seq -s " " $(((100 - $volume))) | sed 's/[0-9]//g')
     # Send the notification
-    ~/dotfiles/notify-send.sh/notify-send.sh "Volume" -u normal \
-        "$volume% $bar$bar_after" --replace-file=/tmp/volume_notification \
+    ~/dotfiles/notify-send.sh/notify-send.sh " Volume" -u normal \
+        "[$bar$bar_after] $volume%" --replace-file=/tmp/volume_notification \
         -a 'pulse' -t 1000
 }
 
@@ -52,8 +52,8 @@ case $1 in
         # Toggle mute
         amixer -D pulse set Master 1+ toggle > /dev/null
         if is_mute ; then
-            ~/dotfiles/notify-send.sh/notify-send.sh 'Volume muted' -u normal \
-                --replace-file=/tmp/volume_notification -a 'pulse' -t 2000
+            ~/dotfiles/notify-send.sh/notify-send.sh ' Mute' -u normal \
+                --replace-file=/tmp/volume_notification -a 'pulse' -t 1000
         else
             send_notification
         fi
