41e81d9f5fa16f9d472cd47d01451f087020f1a4
George Watson <george@georgewatson.me>
2018-12-12 10:58:50 +0000

    Add rtv config

--- ncmpcpp_song_change.sh
+++ ncmpcpp_song_change.sh
@@ -7,4 +7,4 @@ IFS=$'\t' read album artist title \
 ~/dotfiles/notify-send.sh/notify-send.sh \
     --urgency=low --expire-time=5000 --app-name=ncmpcpp \
     --replace-file=/tmp/ncmpcpp_notification \
-    --icon=audio-x-generic "$title" "$artist — $album"
+    --icon=audio-x-generic "$title" "$artist — <i>$album</i>"
