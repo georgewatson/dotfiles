0f0a85ac618fbb9684f12e2959d5ee6b37e6738a
George Watson <george@georgewatson.me>
2018-12-12 09:37:21 +0000

    Detect current sink

--- volume.sh
+++ volume.sh
@@ -9,6 +9,8 @@
 # $./volume.sh mute
 
 function get_volume {
+    SINK=$(pactl list short | grep RUNNING | sed -e \
+        's,^\([0-9][0-9]*\)[^0-9].*,\1,')
     pactl list sinks | grep '^[[:space:]]Volume:' | \
         head -n $(( $SINK + 1 )) | tail -n 1 | \
         sed -e 's,.* \([0-9][0-9]*\)%.*,\1,'
