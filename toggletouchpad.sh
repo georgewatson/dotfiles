40c028cc85b45d87ea671d5ae98e3cd5b2f24040
George Watson <george@georgewatson.me>
2018-12-13 11:57:26 +0000

    Touchpad toggle never worked

--- toggletouchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if xinput list-props 11 | grep "Device Enabled.*:.*1" >/dev/null
-then
-  xinput disable 11
-  notify-send -u low -i mouse "Trackpad disabled"
-else
-  xinput enable 11
-  notify-send -u low -i mouse "Trackpad enabled"
-fi
